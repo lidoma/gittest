9b39a09846942fc644a5b2291576fad345ca6fce
Modammad Davaee <mdavaee@gmail.com>
2014-04-12 18:35:39 +0330

    First commit

---
+++ down.php
@@ -0,0 +1,26 @@
+<?php
+
+    // http://vnfiles.ign.com/ects/css-tricks/VideoCast-122.mp4
+    // http://vnfiles.ign.com/ects/css-tricks/VideoCast-121.mp4
+    // http://vnfiles.ign.com/ects/css-tricks/VideoCast-120.mp4
+
+    $url  = 'http://pdl.vimeocdn.com/65387/496/193682011.mp4?token2=1393416707_920eff461546417365063b64b60003cb&aksessionid=b493009714d137da';
+    $path = 'com/' . 'Authentication Essentials from Laracasts on Vimeo.MP4';
+
+    $fp = fopen($path, 'w');
+
+    $ch = curl_init($url);
+    // curl_setopt($ch, CURLOPT_USERPWD, "m_davaee:3231982");
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+
+    //curl_setopt($ch, CURLOPT_HEADER, 1);
+    //curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    //curl_setopt($ch, CURLOPT_NOBODY, TRUE);
+
+    curl_setopt($ch, CURLOPT_FILE, $fp);
+
+    $data = curl_exec($ch);
+    echo $data;
+    curl_close($ch);
+    fclose($fp);
+?>
